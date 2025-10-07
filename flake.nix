7bb7db5fb9768cde023927638f6fd624cab5fb2e
Robert Cambridge <robert@cambridge.me>
2025-10-07 09:25:27 +0200

    initial structure and ci/cd

---
+++ flake.nix
@@ -0,0 +1,13 @@
+{
+  description = "Flake for multigres-operator";
+
+  # Add all your dependencies here
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable";
+    blueprint.url = "github:numtide/blueprint";
+    blueprint.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  # Load the blueprint
+  outputs = inputs: inputs.blueprint { inherit inputs; };
+}
