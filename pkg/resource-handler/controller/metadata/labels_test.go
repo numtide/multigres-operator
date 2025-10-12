b2632ea46470f74e4e276f54047bfc3db4d7a32d
Ryota <rytswd@gmail.com>
2025-10-12 12:30:28 +0100

    Move metadata test into its own test package

--- pkg/resource-handler/controller/metadata/labels_test.go
+++ pkg/resource-handler/controller/metadata/labels_test.go
@@ -1,9 +1,11 @@
-package metadata
+package metadata_test
 
 import (
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
+
+	"github.com/numtide/multigres-operator/pkg/resource-handler/controller/metadata"
 )
 
 func TestBuildStandardLabels(t *testing.T) {
@@ -18,12 +20,12 @@ func TestBuildStandardLabels(t *testing.T) {
 			componentName: "etcd",
 			cellName:      "cell-1",
 			want: map[string]string{
-				LabelAppName:       AppNameMultigres,
-				LabelAppInstance:   "my-etcd-cluster",
-				LabelAppComponent:  "etcd",
-				LabelAppPartOf:     AppNameMultigres,
-				LabelAppManagedBy:  ManagedByMultigres,
-				LabelMultigresCell: "cell-1",
+				"app.kubernetes.io/name":       "multigres",
+				"app.kubernetes.io/instance":   "my-etcd-cluster",
+				"app.kubernetes.io/component":  "etcd",
+				"app.kubernetes.io/part-of":    "multigres",
+				"app.kubernetes.io/managed-by": "multigres-operator",
+				"multigres.com/cell":           "cell-1",
 			},
 		},
 		"empty cellName uses default": {
@@ -31,12 +33,12 @@ func TestBuildStandardLabels(t *testing.T) {
 			componentName: "gateway",
 			cellName:      "",
 			want: map[string]string{
-				LabelAppName:       AppNameMultigres,
-				LabelAppInstance:   "my-gateway",
-				LabelAppComponent:  "gateway",
-				LabelAppPartOf:     AppNameMultigres,
-				LabelAppManagedBy:  ManagedByMultigres,
-				LabelMultigresCell: DefaultCellName,
+				"app.kubernetes.io/name":       "multigres",
+				"app.kubernetes.io/instance":   "my-gateway",
+				"app.kubernetes.io/component":  "gateway",
+				"app.kubernetes.io/part-of":    "multigres",
+				"app.kubernetes.io/managed-by": "multigres-operator",
+				"multigres.com/cell":           "multigres-global-topo",
 			},
 		},
 		"with cellName should add cell label": {
@@ -44,12 +46,12 @@ func TestBuildStandardLabels(t *testing.T) {
 			componentName: "orch",
 			cellName:      "cell-alpha",
 			want: map[string]string{
-				LabelAppName:       AppNameMultigres,
-				LabelAppInstance:   "my-orch",
-				LabelAppComponent:  "orch",
-				LabelAppPartOf:     AppNameMultigres,
-				LabelAppManagedBy:  ManagedByMultigres,
-				LabelMultigresCell: "cell-alpha",
+				"app.kubernetes.io/name":       "multigres",
+				"app.kubernetes.io/instance":   "my-orch",
+				"app.kubernetes.io/component":  "orch",
+				"app.kubernetes.io/part-of":    "multigres",
+				"app.kubernetes.io/managed-by": "multigres-operator",
+				"multigres.com/cell":           "cell-alpha",
 			},
 		},
 		"empty resourceName": {
@@ -57,12 +59,12 @@ func TestBuildStandardLabels(t *testing.T) {
 			componentName: "pooler",
 			cellName:      "cell-2",
 			want: map[string]string{
-				LabelAppName:       AppNameMultigres,
-				LabelAppInstance:   "",
-				LabelAppComponent:  "pooler",
-				LabelAppPartOf:     AppNameMultigres,
-				LabelAppManagedBy:  ManagedByMultigres,
-				LabelMultigresCell: "cell-2",
+				"app.kubernetes.io/name":       "multigres",
+				"app.kubernetes.io/instance":   "",
+				"app.kubernetes.io/component":  "pooler",
+				"app.kubernetes.io/part-of":    "multigres",
+				"app.kubernetes.io/managed-by": "multigres-operator",
+				"multigres.com/cell":           "cell-2",
 			},
 		},
 		"empty componentName": {
@@ -70,12 +72,12 @@ func TestBuildStandardLabels(t *testing.T) {
 			componentName: "",
 			cellName:      "cell-3",
 			want: map[string]string{
-				LabelAppName:       AppNameMultigres,
-				LabelAppInstance:   "my-resource",
-				LabelAppComponent:  "",
-				LabelAppPartOf:     AppNameMultigres,
-				LabelAppManagedBy:  ManagedByMultigres,
-				LabelMultigresCell: "cell-3",
+				"app.kubernetes.io/name":       "multigres",
+				"app.kubernetes.io/instance":   "my-resource",
+				"app.kubernetes.io/component":  "",
+				"app.kubernetes.io/part-of":    "multigres",
+				"app.kubernetes.io/managed-by": "multigres-operator",
+				"multigres.com/cell":           "cell-3",
 			},
 		},
 		"all empty strings - uses default cell": {
@@ -83,12 +85,12 @@ func TestBuildStandardLabels(t *testing.T) {
 			componentName: "",
 			cellName:      "",
 			want: map[string]string{
-				LabelAppName:       AppNameMultigres,
-				LabelAppInstance:   "",
-				LabelAppComponent:  "",
-				LabelAppPartOf:     AppNameMultigres,
-				LabelAppManagedBy:  ManagedByMultigres,
-				LabelMultigresCell: DefaultCellName,
+				"app.kubernetes.io/name":       "multigres",
+				"app.kubernetes.io/instance":   "",
+				"app.kubernetes.io/component":  "",
+				"app.kubernetes.io/part-of":    "multigres",
+				"app.kubernetes.io/managed-by": "multigres-operator",
+				"multigres.com/cell":           "multigres-global-topo",
 			},
 		},
 		"special characters in names": {
@@ -96,12 +98,12 @@ func TestBuildStandardLabels(t *testing.T) {
 			componentName: "etcd-v3",
 			cellName:      "cell-prod-1",
 			want: map[string]string{
-				LabelAppName:       AppNameMultigres,
-				LabelAppInstance:   "my-resource-123",
-				LabelAppComponent:  "etcd-v3",
-				LabelAppPartOf:     AppNameMultigres,
-				LabelAppManagedBy:  ManagedByMultigres,
-				LabelMultigresCell: "cell-prod-1",
+				"app.kubernetes.io/name":       "multigres",
+				"app.kubernetes.io/instance":   "my-resource-123",
+				"app.kubernetes.io/component":  "etcd-v3",
+				"app.kubernetes.io/part-of":    "multigres",
+				"app.kubernetes.io/managed-by": "multigres-operator",
+				"multigres.com/cell":           "cell-prod-1",
 			},
 		},
 		"long names": {
@@ -109,19 +111,19 @@ func TestBuildStandardLabels(t *testing.T) {
 			componentName: "gateway-proxy-component",
 			cellName:      "cell-production-region-1",
 			want: map[string]string{
-				LabelAppName:       AppNameMultigres,
-				LabelAppInstance:   "very-long-resource-name-with-many-segments",
-				LabelAppComponent:  "gateway-proxy-component",
-				LabelAppPartOf:     AppNameMultigres,
-				LabelAppManagedBy:  ManagedByMultigres,
-				LabelMultigresCell: "cell-production-region-1",
+				"app.kubernetes.io/name":       "multigres",
+				"app.kubernetes.io/instance":   "very-long-resource-name-with-many-segments",
+				"app.kubernetes.io/component":  "gateway-proxy-component",
+				"app.kubernetes.io/part-of":    "multigres",
+				"app.kubernetes.io/managed-by": "multigres-operator",
+				"multigres.com/cell":           "cell-production-region-1",
 			},
 		},
 	}
 
 	for name, tc := range tests {
 		t.Run(name, func(t *testing.T) {
-			got := BuildStandardLabels(tc.resourceName, tc.componentName, tc.cellName)
+			got := metadata.BuildStandardLabels(tc.resourceName, tc.componentName, tc.cellName)
 			if diff := cmp.Diff(tc.want, got); diff != "" {
 				t.Errorf("BuildStandardLabels() mismatch (-want +got):\n%s", diff)
 			}
@@ -137,10 +139,10 @@ func TestMergeLabels(t *testing.T) {
 	}{
 		"both maps populated - standard labels should win on conflicts": {
 			standardLabels: map[string]string{
-				LabelAppName:      AppNameMultigres,
-				LabelAppInstance:  "my-resource",
-				LabelAppComponent: "etcd",
-				LabelAppManagedBy: ManagedByMultigres,
+				"app.kubernetes.io/name":       "multigres",
+				"app.kubernetes.io/instance":   "my-resource",
+				"app.kubernetes.io/component":  "etcd",
+				"app.kubernetes.io/managed-by": "multigres-operator",
 			},
 			customLabels: map[string]string{
 				"app.kubernetes.io/name": "user-app", // conflict: standard should win
@@ -148,12 +150,12 @@ func TestMergeLabels(t *testing.T) {
 				"custom-label-2":         "value2",
 			},
 			want: map[string]string{
-				LabelAppName:      AppNameMultigres, // Standard wins
-				LabelAppInstance:  "my-resource",
-				LabelAppComponent: "etcd",
-				LabelAppManagedBy: ManagedByMultigres,
-				"custom-label-1":  "value1",
-				"custom-label-2":  "value2",
+				"app.kubernetes.io/name":       "multigres", // Standard wins
+				"app.kubernetes.io/instance":   "my-resource",
+				"app.kubernetes.io/component":  "etcd",
+				"app.kubernetes.io/managed-by": "multigres-operator",
+				"custom-label-1":               "value1",
+				"custom-label-2":               "value2",
 			},
 		},
 		"standardLabels nil, customLabels populated": {
@@ -169,15 +171,15 @@ func TestMergeLabels(t *testing.T) {
 		},
 		"customLabels nil, standardLabels populated": {
 			standardLabels: map[string]string{
-				LabelAppName:      AppNameMultigres,
-				LabelAppInstance:  "my-resource",
-				LabelAppComponent: "gateway",
+				"app.kubernetes.io/name":      "multigres",
+				"app.kubernetes.io/instance":  "my-resource",
+				"app.kubernetes.io/component": "gateway",
 			},
 			customLabels: nil,
 			want: map[string]string{
-				LabelAppName:      AppNameMultigres,
-				LabelAppInstance:  "my-resource",
-				LabelAppComponent: "gateway",
+				"app.kubernetes.io/name":      "multigres",
+				"app.kubernetes.io/instance":  "my-resource",
+				"app.kubernetes.io/component": "gateway",
 			},
 		},
 		"both maps nil": {
@@ -187,9 +189,9 @@ func TestMergeLabels(t *testing.T) {
 		},
 		"overlapping keys - standard should override custom": {
 			standardLabels: map[string]string{
-				LabelAppName:      AppNameMultigres,
-				LabelAppInstance:  "resource-1",
-				LabelAppComponent: "orch",
+				"app.kubernetes.io/name":      "multigres",
+				"app.kubernetes.io/instance":  "resource-1",
+				"app.kubernetes.io/component": "orch",
 			},
 			customLabels: map[string]string{
 				"app.kubernetes.io/instance":  "user-defined-name", // conflict: standard should win
@@ -197,16 +199,16 @@ func TestMergeLabels(t *testing.T) {
 				"user-label":                  "user-value",        // no conflict: should be preserved
 			},
 			want: map[string]string{
-				LabelAppName:      AppNameMultigres,
-				LabelAppInstance:  "resource-1", // Standard wins
-				LabelAppComponent: "orch",       // Standard wins
-				"user-label":      "user-value", // Custom preserved
+				"app.kubernetes.io/name":      "multigres",
+				"app.kubernetes.io/instance":  "resource-1", // Standard wins
+				"app.kubernetes.io/component": "orch",       // Standard wins
+				"user-label":                  "user-value", // Custom preserved
 			},
 		},
 		"custom labels with keys not in standard - should be preserved": {
 			standardLabels: map[string]string{
-				LabelAppName:      AppNameMultigres,
-				LabelAppManagedBy: ManagedByMultigres,
+				"app.kubernetes.io/name":       "multigres",
+				"app.kubernetes.io/managed-by": "multigres-operator",
 			},
 			customLabels: map[string]string{
 				"env":             "production",
@@ -215,12 +217,12 @@ func TestMergeLabels(t *testing.T) {
 				"app.custom.io/x": "custom-value",
 			},
 			want: map[string]string{
-				LabelAppName:      AppNameMultigres,
-				LabelAppManagedBy: ManagedByMultigres,
-				"env":             "production",
-				"team":            "platform",
-				"version":         "v1.2.3",
-				"app.custom.io/x": "custom-value",
+				"app.kubernetes.io/name":       "multigres",
+				"app.kubernetes.io/managed-by": "multigres-operator",
+				"app.custom.io/x":              "custom-value",
+				"env":                          "production",
+				"team":                         "platform",
+				"version":                      "v1.2.3",
 			},
 		},
 		"empty standard labels map": {
@@ -234,11 +236,11 @@ func TestMergeLabels(t *testing.T) {
 		},
 		"empty custom labels map": {
 			standardLabels: map[string]string{
-				LabelAppName: AppNameMultigres,
+				"app.kubernetes.io/name": "multigres",
 			},
 			customLabels: map[string]string{},
 			want: map[string]string{
-				LabelAppName: AppNameMultigres,
+				"app.kubernetes.io/name": "multigres",
 			},
 		},
 		"both maps empty": {
@@ -248,11 +250,11 @@ func TestMergeLabels(t *testing.T) {
 		},
 		"all standard labels can override custom labels": {
 			standardLabels: map[string]string{
-				LabelAppName:       AppNameMultigres,
-				LabelAppInstance:   "standard-instance",
-				LabelAppComponent:  "standard-component",
-				LabelAppManagedBy:  ManagedByMultigres,
-				LabelMultigresCell: "standard-cell",
+				"app.kubernetes.io/name":       "multigres",
+				"app.kubernetes.io/instance":   "standard-instance",
+				"app.kubernetes.io/component":  "standard-component",
+				"app.kubernetes.io/managed-by": "multigres-operator",
+				"multigres.com/cell":           "standard-cell",
 			},
 			customLabels: map[string]string{
 				"app.kubernetes.io/name":       "custom-app",       // conflict: standard wins
@@ -262,22 +264,39 @@ func TestMergeLabels(t *testing.T) {
 				"multigres.com/cell":           "custom-cell",      // conflict: standard wins
 			},
 			want: map[string]string{
-				LabelAppName:       AppNameMultigres, // All standard values win
-				LabelAppInstance:   "standard-instance",
-				LabelAppComponent:  "standard-component",
-				LabelAppManagedBy:  ManagedByMultigres,
-				LabelMultigresCell: "standard-cell",
+				"app.kubernetes.io/name":       "multigres", // All standard values win
+				"app.kubernetes.io/instance":   "standard-instance",
+				"app.kubernetes.io/component":  "standard-component",
+				"app.kubernetes.io/managed-by": "multigres-operator",
+				"multigres.com/cell":           "standard-cell",
+			},
+		},
+		"part-of label conflict - standard wins": {
+			standardLabels: map[string]string{
+				"app.kubernetes.io/name":    "multigres",
+				"app.kubernetes.io/part-of": "multigres",
+			},
+			customLabels: map[string]string{
+				"app.kubernetes.io/part-of": "custom-app", // conflict: standard should win
+				"custom-label":              "value",
+			},
+			want: map[string]string{
+				"app.kubernetes.io/name":    "multigres",
+				"app.kubernetes.io/part-of": "multigres", // Standard wins
+				"custom-label":              "value",
 			},
 		},
 		"complex scenario with many labels": {
 			standardLabels: map[string]string{
-				LabelAppName:      AppNameMultigres,
-				LabelAppInstance:  "my-cluster",
-				LabelAppComponent: "pooler",
-				LabelAppManagedBy: ManagedByMultigres,
+				"app.kubernetes.io/name":       "multigres",
+				"app.kubernetes.io/instance":   "my-cluster",
+				"app.kubernetes.io/component":  "pooler",
+				"app.kubernetes.io/part-of":    "multigres",
+				"app.kubernetes.io/managed-by": "multigres-operator",
 			},
 			customLabels: map[string]string{
 				"app.kubernetes.io/component": "user-override", // conflict: standard should win
+				"app.kubernetes.io/part-of":   "custom-part",   // conflict: standard should win
 				"app.custom.io/name":          "custom-app",
 				"kubernetes.io/name":          "k8s-name",
 				"monitoring":                  "enabled",
@@ -287,24 +306,25 @@ func TestMergeLabels(t *testing.T) {
 				"cost-center":                 "engineering",
 			},
 			want: map[string]string{
-				LabelAppName:         AppNameMultigres,
-				LabelAppInstance:     "my-cluster",
-				LabelAppComponent:    "pooler", // Standard wins
-				LabelAppManagedBy:    ManagedByMultigres,
-				"monitoring":         "enabled",
-				"backup":             "true",
-				"tier":               "critical",
-				"owner":              "team-database",
-				"cost-center":        "engineering",
-				"app.custom.io/name": "custom-app",
-				"kubernetes.io/name": "k8s-name",
+				"app.kubernetes.io/name":       "multigres",
+				"app.kubernetes.io/instance":   "my-cluster",
+				"app.kubernetes.io/component":  "pooler",    // Standard wins
+				"app.kubernetes.io/part-of":    "multigres", // Standard wins
+				"app.kubernetes.io/managed-by": "multigres-operator",
+				"app.custom.io/name":           "custom-app",
+				"kubernetes.io/name":           "k8s-name",
+				"monitoring":                   "enabled",
+				"backup":                       "true",
+				"tier":                         "critical",
+				"owner":                        "team-database",
+				"cost-center":                  "engineering",
 			},
 		},
 	}
 
 	for name, tc := range tests {
 		t.Run(name, func(t *testing.T) {
-			got := MergeLabels(tc.standardLabels, tc.customLabels)
+			got := metadata.MergeLabels(tc.standardLabels, tc.customLabels)
 			if diff := cmp.Diff(tc.want, got); diff != "" {
 				t.Errorf("MergeLabels() mismatch (-want +got):\n%s", diff)
 			}
