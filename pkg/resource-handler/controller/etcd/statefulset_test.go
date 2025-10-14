7ab7fd0f5b4259da698bc257cfaf72095b4c4e23
Ryota <rytswd@gmail.com>
2025-10-14 02:21:31 +0100

    Add storage testing

--- pkg/resource-handler/controller/etcd/statefulset_test.go
+++ pkg/resource-handler/controller/etcd/statefulset_test.go
@@ -21,6 +21,10 @@ func boolPtr(b bool) *bool {
 	return &b
 }
 
+func stringPtr(s string) *string {
+	return &s
+}
+
 func TestBuildStatefulSet(t *testing.T) {
 	scheme := runtime.NewScheme()
 	_ = multigresv1alpha1.AddToScheme(scheme)
@@ -226,6 +230,219 @@ func TestBuildStatefulSet(t *testing.T) {
 										corev1.ResourceStorage: resource.MustParse(DefaultStorageSize),
 									},
 								},
+							},
+						},
+					},
+				},
+			},
+		},
+		"custom storage size": {
+			etcd: &multigresv1alpha1.Etcd{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test-etcd",
+					Namespace: "default",
+					UID:       "test-uid",
+				},
+				Spec: multigresv1alpha1.EtcdSpec{
+					StorageSize: "20Gi",
+				},
+			},
+			scheme: scheme,
+			want: &appsv1.StatefulSet{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test-etcd",
+					Namespace: "default",
+					Labels: map[string]string{
+						"app.kubernetes.io/name":       "multigres",
+						"app.kubernetes.io/instance":   "test-etcd",
+						"app.kubernetes.io/component":  "etcd",
+						"app.kubernetes.io/part-of":    "multigres",
+						"app.kubernetes.io/managed-by": "multigres-operator",
+						"multigres.com/cell":           "multigres-global-topo",
+					},
+					OwnerReferences: []metav1.OwnerReference{
+						{
+							APIVersion:         "multigres.com/v1alpha1",
+							Kind:               "Etcd",
+							Name:               "test-etcd",
+							UID:                "test-uid",
+							Controller:         boolPtr(true),
+							BlockOwnerDeletion: boolPtr(true),
+						},
+					},
+				},
+				Spec: appsv1.StatefulSetSpec{
+					ServiceName: "test-etcd-headless",
+					Replicas:    int32Ptr(3),
+					Selector: &metav1.LabelSelector{
+						MatchLabels: map[string]string{
+							"app.kubernetes.io/name":       "multigres",
+							"app.kubernetes.io/instance":   "test-etcd",
+							"app.kubernetes.io/component":  "etcd",
+							"app.kubernetes.io/part-of":    "multigres",
+							"app.kubernetes.io/managed-by": "multigres-operator",
+							"multigres.com/cell":           "multigres-global-topo",
+						},
+					},
+					PodManagementPolicy: appsv1.ParallelPodManagement,
+					UpdateStrategy: appsv1.StatefulSetUpdateStrategy{
+						Type: appsv1.RollingUpdateStatefulSetStrategyType,
+					},
+					Template: corev1.PodTemplateSpec{
+						ObjectMeta: metav1.ObjectMeta{
+							Labels: map[string]string{
+								"app.kubernetes.io/name":       "multigres",
+								"app.kubernetes.io/instance":   "test-etcd",
+								"app.kubernetes.io/component":  "etcd",
+								"app.kubernetes.io/part-of":    "multigres",
+								"app.kubernetes.io/managed-by": "multigres-operator",
+								"multigres.com/cell":           "multigres-global-topo",
+							},
+						},
+						Spec: corev1.PodSpec{
+							Containers: []corev1.Container{
+								{
+									Name:      "etcd",
+									Image:     DefaultImage,
+									Resources: corev1.ResourceRequirements{},
+									Env:       buildEtcdEnv("test-etcd", "default", 3, "test-etcd-headless"),
+									Ports:     buildContainerPorts(nil),
+									VolumeMounts: []corev1.VolumeMount{
+										{
+											Name:      DataVolumeName,
+											MountPath: DataMountPath,
+										},
+									},
+								},
+							},
+						},
+					},
+					VolumeClaimTemplates: []corev1.PersistentVolumeClaim{
+						{
+							ObjectMeta: metav1.ObjectMeta{
+								Name: DataVolumeName,
+							},
+							Spec: corev1.PersistentVolumeClaimSpec{
+								AccessModes: []corev1.PersistentVolumeAccessMode{
+									corev1.ReadWriteOnce,
+								},
+								Resources: corev1.VolumeResourceRequirements{
+									Requests: corev1.ResourceList{
+										corev1.ResourceStorage: resource.MustParse("20Gi"),
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+		"custom VolumeClaimTemplate": {
+			etcd: &multigresv1alpha1.Etcd{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test-etcd",
+					Namespace: "default",
+					UID:       "test-uid",
+				},
+				Spec: multigresv1alpha1.EtcdSpec{
+					VolumeClaimTemplate: &corev1.PersistentVolumeClaimSpec{
+						AccessModes: []corev1.PersistentVolumeAccessMode{
+							corev1.ReadWriteMany,
+						},
+						Resources: corev1.VolumeResourceRequirements{
+							Requests: corev1.ResourceList{
+								corev1.ResourceStorage: resource.MustParse("50Gi"),
+							},
+						},
+						StorageClassName: stringPtr("fast-ssd"),
+					},
+				},
+			},
+			scheme: scheme,
+			want: &appsv1.StatefulSet{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test-etcd",
+					Namespace: "default",
+					Labels: map[string]string{
+						"app.kubernetes.io/name":       "multigres",
+						"app.kubernetes.io/instance":   "test-etcd",
+						"app.kubernetes.io/component":  "etcd",
+						"app.kubernetes.io/part-of":    "multigres",
+						"app.kubernetes.io/managed-by": "multigres-operator",
+						"multigres.com/cell":           "multigres-global-topo",
+					},
+					OwnerReferences: []metav1.OwnerReference{
+						{
+							APIVersion:         "multigres.com/v1alpha1",
+							Kind:               "Etcd",
+							Name:               "test-etcd",
+							UID:                "test-uid",
+							Controller:         boolPtr(true),
+							BlockOwnerDeletion: boolPtr(true),
+						},
+					},
+				},
+				Spec: appsv1.StatefulSetSpec{
+					ServiceName: "test-etcd-headless",
+					Replicas:    int32Ptr(3),
+					Selector: &metav1.LabelSelector{
+						MatchLabels: map[string]string{
+							"app.kubernetes.io/name":       "multigres",
+							"app.kubernetes.io/instance":   "test-etcd",
+							"app.kubernetes.io/component":  "etcd",
+							"app.kubernetes.io/part-of":    "multigres",
+							"app.kubernetes.io/managed-by": "multigres-operator",
+							"multigres.com/cell":           "multigres-global-topo",
+						},
+					},
+					PodManagementPolicy: appsv1.ParallelPodManagement,
+					UpdateStrategy: appsv1.StatefulSetUpdateStrategy{
+						Type: appsv1.RollingUpdateStatefulSetStrategyType,
+					},
+					Template: corev1.PodTemplateSpec{
+						ObjectMeta: metav1.ObjectMeta{
+							Labels: map[string]string{
+								"app.kubernetes.io/name":       "multigres",
+								"app.kubernetes.io/instance":   "test-etcd",
+								"app.kubernetes.io/component":  "etcd",
+								"app.kubernetes.io/part-of":    "multigres",
+								"app.kubernetes.io/managed-by": "multigres-operator",
+								"multigres.com/cell":           "multigres-global-topo",
+							},
+						},
+						Spec: corev1.PodSpec{
+							Containers: []corev1.Container{
+								{
+									Name:      "etcd",
+									Image:     DefaultImage,
+									Resources: corev1.ResourceRequirements{},
+									Env:       buildEtcdEnv("test-etcd", "default", 3, "test-etcd-headless"),
+									Ports:     buildContainerPorts(nil),
+									VolumeMounts: []corev1.VolumeMount{
+										{
+											Name:      DataVolumeName,
+											MountPath: DataMountPath,
+										},
+									},
+								},
+							},
+						},
+					},
+					VolumeClaimTemplates: []corev1.PersistentVolumeClaim{
+						{
+							ObjectMeta: metav1.ObjectMeta{
+								Name: DataVolumeName,
+							},
+							Spec: corev1.PersistentVolumeClaimSpec{
+								AccessModes: []corev1.PersistentVolumeAccessMode{
+									corev1.ReadWriteMany,
+								},
+								Resources: corev1.VolumeResourceRequirements{
+									Requests: corev1.ResourceList{
+										corev1.ResourceStorage: resource.MustParse("50Gi"),
+									},
+								},
+								StorageClassName: stringPtr("fast-ssd"),
 							},
 						},
 					},
