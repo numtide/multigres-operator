241f002e2b16c8ccbd080983b58957bbbc22b016
Ryota <rytswd@gmail.com>
2025-10-17 00:45:04 +0100

    Add envtest setup doc

--- devshell.nix
+++ devshell.nix
@@ -1,5 +1,5 @@
 { pkgs }:
-pkgs.mkShell {
+pkgs.mkShell rec {
   # Add build dependencies
   packages = with pkgs; [
     go
@@ -15,10 +15,13 @@ pkgs.mkShell {
   ];
 
   # Add environment variables
-  env = { };
+  env = {
+    "ENVTEST_K8S_VERSION"= "1.33";  # Default version for Nix users
+  };
 
   # Load custom bash code
   shellHook = ''
-
+    export PRJ_ROOT="$(git rev-parse --show-toplevel 2>/dev/null || pwd)"
+    export KUBEBUILDER_ASSETS="$PRJ_ROOT/bin/k8s/${env.ENVTEST_K8S_VERSION}.0-${pkgs.go.GOOS}-${pkgs.go.GOARCH}"
   '';
 }
