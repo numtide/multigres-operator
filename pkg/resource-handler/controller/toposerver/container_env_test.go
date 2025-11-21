f927e2d884c73177d1cd1875ffeb428dffffa08b
Ryota <rytswd@gmail.com>
2025-11-21 15:42:51 +0000

    Add TopoServer handling controller

---
+++ pkg/resource-handler/controller/toposerver/container_env_test.go
@@ -0,0 +1,328 @@
+package toposerver
+
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	corev1 "k8s.io/api/core/v1"
+)
+
+func TestBuildPodIdentityEnv(t *testing.T) {
+	got := buildPodIdentityEnv()
+
+	want := []corev1.EnvVar{
+		{
+			Name: "POD_NAME",
+			ValueFrom: &corev1.EnvVarSource{
+				FieldRef: &corev1.ObjectFieldSelector{
+					FieldPath: "metadata.name",
+				},
+			},
+		},
+		{
+			Name: "POD_NAMESPACE",
+			ValueFrom: &corev1.EnvVarSource{
+				FieldRef: &corev1.ObjectFieldSelector{
+					FieldPath: "metadata.namespace",
+				},
+			},
+		},
+	}
+
+	if diff := cmp.Diff(want, got); diff != "" {
+		t.Errorf("buildPodIdentityEnv() mismatch (-want +got):\n%s", diff)
+	}
+}
+
+func TestBuildEtcdConfigEnv(t *testing.T) {
+	tests := map[string]struct {
+		toposerverName string
+		serviceName    string
+		namespace      string
+		want           []corev1.EnvVar
+	}{
+		"basic configuration": {
+			toposerverName: "my-toposerver",
+			serviceName:    "my-toposerver-headless",
+			namespace:      "default",
+			want: []corev1.EnvVar{
+				{Name: "ETCD_NAME", Value: "$(POD_NAME)"},
+				{Name: "ETCD_DATA_DIR", Value: "/var/lib/etcd"},
+				{Name: "ETCD_LISTEN_CLIENT_URLS", Value: "http://0.0.0.0:2379"},
+				{Name: "ETCD_LISTEN_PEER_URLS", Value: "http://0.0.0.0:2380"},
+				{
+					Name:  "ETCD_ADVERTISE_CLIENT_URLS",
+					Value: "http://$(POD_NAME).my-toposerver-headless.$(POD_NAMESPACE).svc.cluster.local:2379",
+				},
+				{
+					Name:  "ETCD_INITIAL_ADVERTISE_PEER_URLS",
+					Value: "http://$(POD_NAME).my-toposerver-headless.$(POD_NAMESPACE).svc.cluster.local:2380",
+				},
+				{Name: "ETCD_INITIAL_CLUSTER_STATE", Value: "new"},
+				{Name: "ETCD_INITIAL_CLUSTER_TOKEN", Value: "my-toposerver"},
+			},
+		},
+		"different namespace": {
+			toposerverName: "test-toposerver",
+			serviceName:    "test-toposerver-headless",
+			namespace:      "production",
+			want: []corev1.EnvVar{
+				{Name: "ETCD_NAME", Value: "$(POD_NAME)"},
+				{Name: "ETCD_DATA_DIR", Value: "/var/lib/etcd"},
+				{Name: "ETCD_LISTEN_CLIENT_URLS", Value: "http://0.0.0.0:2379"},
+				{Name: "ETCD_LISTEN_PEER_URLS", Value: "http://0.0.0.0:2380"},
+				{
+					Name:  "ETCD_ADVERTISE_CLIENT_URLS",
+					Value: "http://$(POD_NAME).test-toposerver-headless.$(POD_NAMESPACE).svc.cluster.local:2379",
+				},
+				{
+					Name:  "ETCD_INITIAL_ADVERTISE_PEER_URLS",
+					Value: "http://$(POD_NAME).test-toposerver-headless.$(POD_NAMESPACE).svc.cluster.local:2380",
+				},
+				{Name: "ETCD_INITIAL_CLUSTER_STATE", Value: "new"},
+				{Name: "ETCD_INITIAL_CLUSTER_TOKEN", Value: "test-toposerver"},
+			},
+		},
+		"long names": {
+			toposerverName: "very-long-toposerver-cluster-name",
+			serviceName:    "very-long-toposerver-cluster-name-headless",
+			namespace:      "kube-system",
+			want: []corev1.EnvVar{
+				{Name: "ETCD_NAME", Value: "$(POD_NAME)"},
+				{Name: "ETCD_DATA_DIR", Value: "/var/lib/etcd"},
+				{Name: "ETCD_LISTEN_CLIENT_URLS", Value: "http://0.0.0.0:2379"},
+				{Name: "ETCD_LISTEN_PEER_URLS", Value: "http://0.0.0.0:2380"},
+				{
+					Name:  "ETCD_ADVERTISE_CLIENT_URLS",
+					Value: "http://$(POD_NAME).very-long-toposerver-cluster-name-headless.$(POD_NAMESPACE).svc.cluster.local:2379",
+				},
+				{
+					Name:  "ETCD_INITIAL_ADVERTISE_PEER_URLS",
+					Value: "http://$(POD_NAME).very-long-toposerver-cluster-name-headless.$(POD_NAMESPACE).svc.cluster.local:2380",
+				},
+				{Name: "ETCD_INITIAL_CLUSTER_STATE", Value: "new"},
+				{Name: "ETCD_INITIAL_CLUSTER_TOKEN", Value: "very-long-toposerver-cluster-name"},
+			},
+		},
+	}
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			got := buildEtcdConfigEnv(tc.toposerverName, tc.serviceName, tc.namespace)
+			if diff := cmp.Diff(tc.want, got); diff != "" {
+				t.Errorf("buildEtcdConfigEnv() mismatch (-want +got):\n%s", diff)
+			}
+		})
+	}
+}
+
+func TestBuildEtcdClusterPeerList(t *testing.T) {
+	tests := map[string]struct {
+		toposerverName string
+		serviceName    string
+		namespace      string
+		replicas       int32
+		want           string
+	}{
+		"single replica": {
+			toposerverName: "my-toposerver",
+			serviceName:    "my-toposerver-headless",
+			namespace:      "default",
+			replicas:       1,
+			want:           "my-toposerver-0=http://my-toposerver-0.my-toposerver-headless.default.svc.cluster.local:2380",
+		},
+		"three replicas (typical HA)": {
+			toposerverName: "my-toposerver",
+			serviceName:    "my-toposerver-headless",
+			namespace:      "default",
+			replicas:       3,
+			want:           "my-toposerver-0=http://my-toposerver-0.my-toposerver-headless.default.svc.cluster.local:2380,my-toposerver-1=http://my-toposerver-1.my-toposerver-headless.default.svc.cluster.local:2380,my-toposerver-2=http://my-toposerver-2.my-toposerver-headless.default.svc.cluster.local:2380",
+		},
+		"five replicas": {
+			toposerverName: "toposerver-prod",
+			serviceName:    "toposerver-prod-headless",
+			namespace:      "production",
+			replicas:       5,
+			want:           "toposerver-prod-0=http://toposerver-prod-0.toposerver-prod-headless.production.svc.cluster.local:2380,toposerver-prod-1=http://toposerver-prod-1.toposerver-prod-headless.production.svc.cluster.local:2380,toposerver-prod-2=http://toposerver-prod-2.toposerver-prod-headless.production.svc.cluster.local:2380,toposerver-prod-3=http://toposerver-prod-3.toposerver-prod-headless.production.svc.cluster.local:2380,toposerver-prod-4=http://toposerver-prod-4.toposerver-prod-headless.production.svc.cluster.local:2380",
+		},
+		"zero replicas": {
+			toposerverName: "my-toposerver",
+			serviceName:    "my-toposerver-headless",
+			namespace:      "default",
+			replicas:       0,
+			want:           "",
+		},
+		"negative replicas": {
+			toposerverName: "my-toposerver",
+			serviceName:    "my-toposerver-headless",
+			namespace:      "default",
+			replicas:       -1,
+			want:           "",
+		},
+		"different namespace": {
+			toposerverName: "kube-toposerver",
+			serviceName:    "kube-toposerver-headless",
+			namespace:      "kube-system",
+			replicas:       3,
+			want:           "kube-toposerver-0=http://kube-toposerver-0.kube-toposerver-headless.kube-system.svc.cluster.local:2380,kube-toposerver-1=http://kube-toposerver-1.kube-toposerver-headless.kube-system.svc.cluster.local:2380,kube-toposerver-2=http://kube-toposerver-2.kube-toposerver-headless.kube-system.svc.cluster.local:2380",
+		},
+	}
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			got := buildEtcdClusterPeerList(tc.toposerverName, tc.serviceName, tc.namespace, tc.replicas)
+			if got != tc.want {
+				t.Errorf("buildEtcdClusterPeerList() = %v, want %v", got, tc.want)
+			}
+		})
+	}
+}
+
+func TestBuildContainerEnv(t *testing.T) {
+	tests := map[string]struct {
+		toposerverName string
+		namespace      string
+		replicas       int32
+		serviceName    string
+		want           []corev1.EnvVar
+	}{
+		"complete environment with 3 replicas": {
+			toposerverName: "my-toposerver",
+			namespace:      "default",
+			replicas:       3,
+			serviceName:    "my-toposerver-headless",
+			want: []corev1.EnvVar{
+				{
+					Name: "POD_NAME",
+					ValueFrom: &corev1.EnvVarSource{
+						FieldRef: &corev1.ObjectFieldSelector{
+							FieldPath: "metadata.name",
+						},
+					},
+				},
+				{
+					Name: "POD_NAMESPACE",
+					ValueFrom: &corev1.EnvVarSource{
+						FieldRef: &corev1.ObjectFieldSelector{
+							FieldPath: "metadata.namespace",
+						},
+					},
+				},
+				{Name: "ETCD_NAME", Value: "$(POD_NAME)"},
+				{Name: "ETCD_DATA_DIR", Value: "/var/lib/etcd"},
+				{Name: "ETCD_LISTEN_CLIENT_URLS", Value: "http://0.0.0.0:2379"},
+				{Name: "ETCD_LISTEN_PEER_URLS", Value: "http://0.0.0.0:2380"},
+				{
+					Name:  "ETCD_ADVERTISE_CLIENT_URLS",
+					Value: "http://$(POD_NAME).my-toposerver-headless.$(POD_NAMESPACE).svc.cluster.local:2379",
+				},
+				{
+					Name:  "ETCD_INITIAL_ADVERTISE_PEER_URLS",
+					Value: "http://$(POD_NAME).my-toposerver-headless.$(POD_NAMESPACE).svc.cluster.local:2380",
+				},
+				{Name: "ETCD_INITIAL_CLUSTER_STATE", Value: "new"},
+				{Name: "ETCD_INITIAL_CLUSTER_TOKEN", Value: "my-toposerver"},
+				{
+					Name:  "ETCD_INITIAL_CLUSTER",
+					Value: "my-toposerver-0=http://my-toposerver-0.my-toposerver-headless.default.svc.cluster.local:2380,my-toposerver-1=http://my-toposerver-1.my-toposerver-headless.default.svc.cluster.local:2380,my-toposerver-2=http://my-toposerver-2.my-toposerver-headless.default.svc.cluster.local:2380",
+				},
+			},
+		},
+		"single replica": {
+			toposerverName: "test-toposerver",
+			namespace:      "test",
+			replicas:       1,
+			serviceName:    "test-toposerver-headless",
+			want: []corev1.EnvVar{
+				{
+					Name: "POD_NAME",
+					ValueFrom: &corev1.EnvVarSource{
+						FieldRef: &corev1.ObjectFieldSelector{
+							FieldPath: "metadata.name",
+						},
+					},
+				},
+				{
+					Name: "POD_NAMESPACE",
+					ValueFrom: &corev1.EnvVarSource{
+						FieldRef: &corev1.ObjectFieldSelector{
+							FieldPath: "metadata.namespace",
+						},
+					},
+				},
+				{Name: "ETCD_NAME", Value: "$(POD_NAME)"},
+				{Name: "ETCD_DATA_DIR", Value: "/var/lib/etcd"},
+				{Name: "ETCD_LISTEN_CLIENT_URLS", Value: "http://0.0.0.0:2379"},
+				{Name: "ETCD_LISTEN_PEER_URLS", Value: "http://0.0.0.0:2380"},
+				{
+					Name:  "ETCD_ADVERTISE_CLIENT_URLS",
+					Value: "http://$(POD_NAME).test-toposerver-headless.$(POD_NAMESPACE).svc.cluster.local:2379",
+				},
+				{
+					Name:  "ETCD_INITIAL_ADVERTISE_PEER_URLS",
+					Value: "http://$(POD_NAME).test-toposerver-headless.$(POD_NAMESPACE).svc.cluster.local:2380",
+				},
+				// Cluster setup won't happen in a single cluster, and these
+				// env variables are only used at startup.
+				{Name: "ETCD_INITIAL_CLUSTER_STATE", Value: "new"},
+				{Name: "ETCD_INITIAL_CLUSTER_TOKEN", Value: "test-toposerver"},
+				{
+					Name:  "ETCD_INITIAL_CLUSTER",
+					Value: "test-toposerver-0=http://test-toposerver-0.test-toposerver-headless.test.svc.cluster.local:2380",
+				},
+			},
+		},
+		"zero replicas - no ETCD_INITIAL_CLUSTER": {
+			toposerverName: "empty-toposerver",
+			namespace:      "default",
+			replicas:       0,
+			serviceName:    "empty-toposerver-headless",
+			want: []corev1.EnvVar{
+				{
+					Name: "POD_NAME",
+					ValueFrom: &corev1.EnvVarSource{
+						FieldRef: &corev1.ObjectFieldSelector{
+							FieldPath: "metadata.name",
+						},
+					},
+				},
+				{
+					Name: "POD_NAMESPACE",
+					ValueFrom: &corev1.EnvVarSource{
+						FieldRef: &corev1.ObjectFieldSelector{
+							FieldPath: "metadata.namespace",
+						},
+					},
+				},
+				{Name: "ETCD_NAME", Value: "$(POD_NAME)"},
+				{Name: "ETCD_DATA_DIR", Value: "/var/lib/etcd"},
+				{Name: "ETCD_LISTEN_CLIENT_URLS", Value: "http://0.0.0.0:2379"},
+				{Name: "ETCD_LISTEN_PEER_URLS", Value: "http://0.0.0.0:2380"},
+				{
+					Name:  "ETCD_ADVERTISE_CLIENT_URLS",
+					Value: "http://$(POD_NAME).empty-toposerver-headless.$(POD_NAMESPACE).svc.cluster.local:2379",
+				},
+				{
+					Name:  "ETCD_INITIAL_ADVERTISE_PEER_URLS",
+					Value: "http://$(POD_NAME).empty-toposerver-headless.$(POD_NAMESPACE).svc.cluster.local:2380",
+				},
+				// Cluster setup won't happen in a single cluster, and these
+				// env variables are only used at startup. In case of scaling up
+				// from zero replica, the updated env variable will be picked up
+				// correctly, and thus an empty variable like this will be OK.
+				{Name: "ETCD_INITIAL_CLUSTER_STATE", Value: "new"},
+				{Name: "ETCD_INITIAL_CLUSTER_TOKEN", Value: "empty-toposerver"},
+				{Name: "ETCD_INITIAL_CLUSTER"},
+			},
+		},
+	}
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			got := buildContainerEnv(tc.toposerverName, tc.namespace, tc.replicas, tc.serviceName)
+			if diff := cmp.Diff(tc.want, got); diff != "" {
+				t.Errorf("BuildContainerEnv() mismatch (-want +got):\n%s", diff)
+			}
+		})
+	}
+}
---
+++ pkg/resource-handler/controller/toposerver/integration_test.go
@@ -0,0 +1,266 @@
+//go:build integration
+// +build integration
+
+package toposerver_test
+
+import (
+	"testing"
+
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/util/intstr"
+	"k8s.io/utils/ptr"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller"
+
+	multigresv1alpha1 "github.com/numtide/multigres-operator/api/v1alpha1"
+	"github.com/numtide/multigres-operator/pkg/resource-handler/controller/testutil"
+	toposervercontroller "github.com/numtide/multigres-operator/pkg/resource-handler/controller/toposerver"
+)
+
+func TestSetupWithManager(t *testing.T) {
+	scheme := runtime.NewScheme()
+	_ = multigresv1alpha1.AddToScheme(scheme)
+	_ = appsv1.AddToScheme(scheme)
+	_ = corev1.AddToScheme(scheme)
+
+	cfg := testutil.SetUpEnvtest(t)
+	mgr := testutil.SetUpManager(t, cfg, scheme)
+	testutil.StartManager(t, mgr)
+
+	if err := (&toposervercontroller.TopoServerReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		t.Fatalf("Failed to create controller, %v", err)
+	}
+}
+
+func TestTopoServerReconciliation(t *testing.T) {
+	t.Parallel()
+
+	scheme := runtime.NewScheme()
+	_ = multigresv1alpha1.AddToScheme(scheme)
+	_ = appsv1.AddToScheme(scheme)
+	_ = corev1.AddToScheme(scheme)
+
+	tests := map[string]struct {
+		toposerver      *multigresv1alpha1.TopoServer
+		existingObjects []client.Object
+		failureConfig   *testutil.FailureConfig
+		wantResources   []client.Object
+		wantErr         bool
+		wantRequeue     bool
+		assertFunc      func(t *testing.T, c client.Client, toposerver *multigresv1alpha1.TopoServer)
+	}{
+		"simple toposerver input": {
+			toposerver: &multigresv1alpha1.TopoServer{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test-toposerver",
+					Namespace: "default",
+				},
+				Spec: multigresv1alpha1.TopoServerChildSpec{
+					RootPath: "/vitess/global",
+				},
+			},
+			wantResources: []client.Object{
+				&appsv1.StatefulSet{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:            "test-toposerver",
+						Namespace:       "default",
+						Labels:          toposerverLabels(t, "test-toposerver"),
+						OwnerReferences: toposerverOwnerRefs(t, "test-toposerver"),
+					},
+					Spec: appsv1.StatefulSetSpec{
+						Replicas:    ptr.To(int32(3)),
+						ServiceName: "test-toposerver-headless",
+						Selector: &metav1.LabelSelector{
+							MatchLabels: toposerverLabels(t, "test-toposerver"),
+						},
+						Template: corev1.PodTemplateSpec{
+							ObjectMeta: metav1.ObjectMeta{
+								Labels: toposerverLabels(t, "test-toposerver"),
+							},
+							Spec: corev1.PodSpec{
+								Containers: []corev1.Container{
+									{
+										Name:  "etcd",
+										Image: "gcr.io/etcd-development/etcd:v3.5.9",
+										Ports: []corev1.ContainerPort{
+											tcpPort(t, "client", 2379),
+											tcpPort(t, "peer", 2380),
+										},
+										Env: []corev1.EnvVar{
+											{
+												Name: "POD_NAME",
+												ValueFrom: &corev1.EnvVarSource{
+													FieldRef: &corev1.ObjectFieldSelector{
+														APIVersion: "v1",
+														FieldPath:  "metadata.name",
+													},
+												},
+											},
+											{
+												Name: "POD_NAMESPACE",
+												ValueFrom: &corev1.EnvVarSource{
+													FieldRef: &corev1.ObjectFieldSelector{
+														APIVersion: "v1",
+														FieldPath:  "metadata.namespace",
+													},
+												},
+											},
+											{Name: "ETCD_NAME", Value: "$(POD_NAME)"},
+											{Name: "ETCD_DATA_DIR", Value: "/var/lib/etcd"},
+											{Name: "ETCD_LISTEN_CLIENT_URLS", Value: "http://0.0.0.0:2379"},
+											{Name: "ETCD_LISTEN_PEER_URLS", Value: "http://0.0.0.0:2380"},
+											{Name: "ETCD_ADVERTISE_CLIENT_URLS", Value: "http://$(POD_NAME).test-toposerver-headless.$(POD_NAMESPACE).svc.cluster.local:2379"},
+											{Name: "ETCD_INITIAL_ADVERTISE_PEER_URLS", Value: "http://$(POD_NAME).test-toposerver-headless.$(POD_NAMESPACE).svc.cluster.local:2380"},
+											{Name: "ETCD_INITIAL_CLUSTER_STATE", Value: "new"},
+											{Name: "ETCD_INITIAL_CLUSTER_TOKEN", Value: "test-toposerver"},
+											{Name: "ETCD_INITIAL_CLUSTER", Value: "test-toposerver-0=http://test-toposerver-0.test-toposerver-headless.default.svc.cluster.local:2380,test-toposerver-1=http://test-toposerver-1.test-toposerver-headless.default.svc.cluster.local:2380,test-toposerver-2=http://test-toposerver-2.test-toposerver-headless.default.svc.cluster.local:2380"},
+										},
+										VolumeMounts: []corev1.VolumeMount{
+											{Name: "data", MountPath: "/var/lib/etcd"},
+										},
+									},
+								},
+							},
+						},
+						VolumeClaimTemplates: []corev1.PersistentVolumeClaim{
+							{
+								ObjectMeta: metav1.ObjectMeta{Name: "data"},
+								Spec: corev1.PersistentVolumeClaimSpec{
+									AccessModes: []corev1.PersistentVolumeAccessMode{corev1.ReadWriteOnce},
+									Resources: corev1.VolumeResourceRequirements{
+										Requests: corev1.ResourceList{
+											corev1.ResourceStorage: resource.MustParse("10Gi"),
+										},
+									},
+									VolumeMode: ptr.To(corev1.PersistentVolumeFilesystem),
+								},
+								Status: corev1.PersistentVolumeClaimStatus{
+									Phase: corev1.ClaimPending,
+								},
+							},
+						},
+					},
+				},
+				&corev1.Service{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:            "test-toposerver",
+						Namespace:       "default",
+						Labels:          toposerverLabels(t, "test-toposerver"),
+						OwnerReferences: toposerverOwnerRefs(t, "test-toposerver"),
+					},
+					Spec: corev1.ServiceSpec{
+						Type: corev1.ServiceTypeClusterIP,
+						Ports: []corev1.ServicePort{
+							tcpServicePort(t, "client", 2379),
+						},
+						Selector: toposerverLabels(t, "test-toposerver"),
+					},
+				},
+				&corev1.Service{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:            "test-toposerver-headless",
+						Namespace:       "default",
+						Labels:          toposerverLabels(t, "test-toposerver"),
+						OwnerReferences: toposerverOwnerRefs(t, "test-toposerver"),
+					},
+					Spec: corev1.ServiceSpec{
+						Type:      corev1.ServiceTypeClusterIP,
+						ClusterIP: corev1.ClusterIPNone,
+						Ports: []corev1.ServicePort{
+							tcpServicePort(t, "client", 2379),
+							tcpServicePort(t, "peer", 2380),
+						},
+						Selector:                 toposerverLabels(t, "test-toposerver"),
+						PublishNotReadyAddresses: true,
+					},
+				},
+			},
+		},
+	}
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+			ctx := t.Context()
+			mgr := testutil.SetUpEnvtestManager(t, scheme)
+
+			watcher := testutil.NewResourceWatcher(t, ctx, mgr,
+				testutil.WithCmpOpts(
+					testutil.IgnoreMetaRuntimeFields(),
+					testutil.IgnoreServiceRuntimeFields(),
+					testutil.IgnoreStatefulSetRuntimeFields(),
+					testutil.IgnorePodSpecDefaults(),
+					testutil.IgnoreStatefulSetSpecDefaults(),
+				),
+				testutil.WithExtraResource(&multigresv1alpha1.TopoServer{}),
+			)
+			client := mgr.GetClient()
+
+			toposerverReconciler := &toposervercontroller.TopoServerReconciler{
+				Client: mgr.GetClient(),
+				Scheme: mgr.GetScheme(),
+			}
+			if err := toposerverReconciler.SetupWithManager(mgr, controller.Options{
+				// Needed for the parallel test runs
+				SkipNameValidation: ptr.To(true),
+			}); err != nil {
+				t.Fatalf("Failed to create controller, %v", err)
+			}
+
+			if err := client.Create(ctx, tc.toposerver); err != nil {
+				t.Fatalf("Failed to create the initial item, %v", err)
+			}
+
+			if err := watcher.WaitForMatch(tc.wantResources...); err != nil {
+				t.Errorf("Resources mismatch:\n%v", err)
+			}
+		})
+	}
+
+}
+
+// Test helpers
+
+// toposerverLabels returns standard labels for toposerver resources in tests
+func toposerverLabels(t testing.TB, instanceName string) map[string]string {
+	t.Helper()
+	return map[string]string{
+		"app.kubernetes.io/component":  "toposerver",
+		"app.kubernetes.io/instance":   instanceName,
+		"app.kubernetes.io/managed-by": "multigres-operator",
+		"app.kubernetes.io/name":       "multigres",
+		"app.kubernetes.io/part-of":    "multigres",
+		"multigres.com/cell":           "multigres-global-topo",
+	}
+}
+
+// toposerverOwnerRefs returns owner references for a TopoServer resource
+func toposerverOwnerRefs(t testing.TB, toposerverName string) []metav1.OwnerReference {
+	t.Helper()
+	return []metav1.OwnerReference{{
+		APIVersion:         "multigres.com/v1alpha1",
+		Kind:               "TopoServer",
+		Name:               toposerverName,
+		Controller:         ptr.To(true),
+		BlockOwnerDeletion: ptr.To(true),
+	}}
+}
+
+// tcpPort creates a simple TCP container port
+func tcpPort(t testing.TB, name string, port int32) corev1.ContainerPort {
+	t.Helper()
+	return corev1.ContainerPort{Name: name, ContainerPort: port, Protocol: corev1.ProtocolTCP}
+}
+
+// tcpServicePort creates a TCP service port with named target
+func tcpServicePort(t testing.TB, name string, port int32) corev1.ServicePort {
+	t.Helper()
+	return corev1.ServicePort{Name: name, Port: port, TargetPort: intstr.FromString(name), Protocol: corev1.ProtocolTCP}
+}
