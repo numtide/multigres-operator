fc7b5413d4c08d454b2c600244f262d881c8e209
Ryota <rytswd@gmail.com>
2025-12-12 23:45:43 +0000

    Remove testutil from resource-handler

--- pkg/resource-handler/controller/testutil/resource_watcher_diffs_summary.go
+++
@@ -1,8 +0,0 @@
-//go:build !verbose
-
-package testutil
-
-// showDiffs controls whether detailed diffs are logged when resources don't match.
-// By default, only summary messages are shown.
-// To enable detailed diffs, build with the "verbose" tag: go test -tags=verbose
-const showDiffs = false
--- pkg/resource-handler/controller/testutil/resource_watcher_diffs_verbose.go
+++
@@ -1,7 +0,0 @@
-//go:build verbose
-
-package testutil
-
-// showDiffs controls whether detailed diffs are logged when resources don't match.
-// This file is compiled when building with the "verbose" tag: go test -tags=verbose
-const showDiffs = true
