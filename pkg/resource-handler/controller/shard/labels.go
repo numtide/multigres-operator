9388d20dc019881b1140e062b4742b6f566fb0a2
Ryota <rytswd@gmail.com>
2025-12-13 03:31:30 +0000

    Move pool label definitions to labels.go

--- pkg/resource-handler/controller/shard/labels.go
+++ pkg/resource-handler/controller/shard/labels.go
@@ -1,23 +1,34 @@
 package shard
 
-// // buildLabels creates standard labels for Shard related resources.
-// // Uses the pool's cell names from the Cell field.
-// func buildLabels(
-// 	shard *multigresv1alpha1.Shard,
-// 	poolName string,
-// ) map[string]string {
-// 	cs := []string{}
-// 	for _, p := range shard.Spec.Pools {
-// 		cs = append(cs, p.Cell)
-// 	}
-// 	cellName := strings.Join(cs, ",")
-// 	if cellName == "" {
-// 		cellName = metadata.DefaultCellName
-// 	}
+import (
+	multigresv1alpha1 "github.com/numtide/multigres-operator/api/v1alpha1"
+	"github.com/numtide/multigres-operator/pkg/resource-handler/controller/metadata"
+)
 
-// 	labels := metadata.BuildStandardLabels(poolName, PoolComponentName)
-// 	// Merge any labels associated from Shard.
-// 	maps.Copy(labels, shard.GetObjectMeta().GetLabels())
+// buildPoolLabels creates standard labels for pool resources, and uses the
+// pool's database, table group, and cell details. Any additional labels are
+// also merged, while keeping the main labels.
+func buildPoolLabels(
+	shard *multigresv1alpha1.Shard,
+	poolName string,
+	poolSpec multigresv1alpha1.ShardPoolSpec,
+) map[string]string {
+	fullPoolName := buildPoolName(shard.Name, poolName)
+	cellName := poolSpec.Cell
+	if cellName == "" {
+		cellName = metadata.DefaultCellName
+	}
 
-// 	return labels
-// }
+	// TODO: Remove this once we figure what to do with the cell name.
+	_ = cellName
+
+	labels := metadata.BuildStandardLabels(fullPoolName, PoolComponentName)
+	// TODO: Add multigres.com/* labels after finalizing label design:
+	// metadata.AddCellLabel(labels, cellName)
+	// metadata.AddDatabaseLabel(labels, poolSpec.Database)
+	// metadata.AddTableGroupLabel(labels, poolSpec.TableGroup)
+
+	metadata.MergeLabels(labels, shard.GetObjectMeta().GetLabels())
+
+	return labels
+}
--- pkg/resource-handler/controller/shard/pool_service.go
+++ pkg/resource-handler/controller/shard/pool_service.go
@@ -9,7 +9,6 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 
 	multigresv1alpha1 "github.com/numtide/multigres-operator/api/v1alpha1"
-	"github.com/numtide/multigres-operator/pkg/resource-handler/controller/metadata"
 )
 
 const (
@@ -52,31 +51,4 @@ func BuildPoolHeadlessService(
 // buildPoolName generates a consistent name for pool resources.
 func buildPoolName(shardName, poolName string) string {
 	return fmt.Sprintf("%s-pool-%s", shardName, poolName)
-}
-
-// buildPoolLabels creates standard labels for pool resources, and uses the
-// pool's database, table group, and cell details. Any additional labels are
-// also merged, while keeping the main labels.
-func buildPoolLabels(
-	shard *multigresv1alpha1.Shard,
-	poolName string,
-	poolSpec multigresv1alpha1.ShardPoolSpec,
-) map[string]string {
-	fullPoolName := buildPoolName(shard.Name, poolName)
-	cellName := poolSpec.Cell
-	if cellName == "" {
-		cellName = metadata.DefaultCellName
-	}
-	// TODO: Remove this once we figure what to do with the cell name.
-	_ = cellName
-
-	labels := metadata.BuildStandardLabels(fullPoolName, PoolComponentName)
-	// TODO: Add multigres.com/* labels after finalizing label design:
-	// metadata.AddCellLabel(labels, cellName)
-	// metadata.AddDatabaseLabel(labels, poolSpec.Database)
-	// metadata.AddTableGroupLabel(labels, poolSpec.TableGroup)
-
-	metadata.MergeLabels(labels, shard.GetObjectMeta().GetLabels())
-
-	return labels
 }
